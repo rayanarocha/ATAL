c67d198ecd990521ed0ca8fb8c7f71cd1a251ead
Rayana Souza <rayana.azus@gmail.com>
2016-02-21 20:47:29 -0300

    Restaurante

---
+++ Restaurante.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+
+struct Endereco {
+		string rua;
+		string bairro;
+		string numero;
+		struct Endereco *prox;
+	} endereco;
+	
+	
+struct Restaurante {
+		string nome;
+		Endereco endereco;
+		string tipo_comida;
+		double nota;
+		Struct Restaurante *prox;
+	}restaurante;
+	
+void inserir
+int main(int argc, char *argv[]) {
+	
+	
+	
+	return 0;
+}
